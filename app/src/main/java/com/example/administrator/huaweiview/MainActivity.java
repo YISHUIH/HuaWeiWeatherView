6cf24275afa87470ce80d82b81b329b53a9524f0
YISHUIH <1070379530@qq.com>
2017-04-21 18:01:33 +0800

    主界面简单逻辑

--- app/src/main/java/com/example/administrator/huaweiview/MainActivity.java
+++ app/src/main/java/com/example/administrator/huaweiview/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.administrator.huaweiview;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv;
     MyView mv;
+    LinearLayout ll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv= (TextView) findViewById(R.id.tv);
         mv= (MyView) findViewById(R.id.mv);
+        mv.change(200);
+       ll= (LinearLayout) findViewById(R.id.ll);
         tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,5 +27,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mv.setOnAngleColorListener(onAngleColorListener);
+        mv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mv.change(200);
+            }
+        });
     }
+    private MyView.OnAngleColorListener onAngleColorListener=new MyView.OnAngleColorListener() {
+        @Override
+        public void onAngleColorListener(int red, int green) {
+            Color color=new Color();
+            int c=color.argb(150, red, green, 0);
+            ll.setBackgroundColor(c);
+        }
+    };
 }
